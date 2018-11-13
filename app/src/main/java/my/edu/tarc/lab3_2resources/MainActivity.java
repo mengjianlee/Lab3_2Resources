8e4e4d6557c7c98bf577e80dd23706d9ac4517ff
mengjian <mengjianlee@gmail.com>
2018-11-13 13:04:27 +0800

    first

---
+++ app/src/main/java/my/edu/tarc/lab3_2resources/MainActivity.java
@@ -0,0 +1,21 @@
+package my.edu.tarc.lab3_2resources;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void showMessage(View view){
+        textView.setText(getString(R.string.greeting));
+    }
+
+}
